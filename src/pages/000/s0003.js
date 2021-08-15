8f7525896b0aad2a0c1a32755bf7efd5189a9c20
Daichi1983 <77876382+Daichi1983@users.noreply.github.com>
2021-08-16 07:36:36 +0900

    210816 updated

---
+++ src/pages/000/s0003.js
@@ -0,0 +1,45 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <Seo title="関口存男文例集総目次" />
+<div class="container">
+
+<div class="row"><div class="col-2">袋番号</div><div class="col-8">タイトル</div><div class="col-2">ページ</div></div>
+<div class="row"><div class="col-2">02-1B</div><div class="col-8"><Link to="../../002/s0001">Nomen agentisの用法</Link></div><div class="col-2">1</div></div>
+<div class="row"><div class="col-2">02-2B</div><div class="col-8"><Link to="../../002/s0002">”子”ling, Eektron, Isolator 格子、穴ぼっこ、娘っこ</Link></div><div class="col-2">2-5</div></div>
+<div class="row"><div class="col-2">02-3B</div><div class="col-8"><Link to="../../002/s0006">貶詞、附: launenhafte bezeichnung</Link></div><div class="col-2">6-59</div></div>
+<div class="row"><div class="col-2">02-4B</div><div class="col-8"><Link to="../../002/s0060">縮小名詞 Diminutiva Koseform</Link></div><div class="col-2">60-148</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0069">［01］ =chen, =lein 両方あるもの</Link></div><div class="col-2">69</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0079">［02］一方よりなきもの Äuglein</Link></div><div class="col-2">79</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0082">［03］ =chen, =leinのつけ方の不規則 変音せざるもの</Link></div><div class="col-2">82</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0108">［04］ =el, (とumlaut) =el, (ohne umlaut)</Link></div><div class="col-2">108</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0113">［05］スイスの =li, =i.</Link></div><div class="col-2">113</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0115">［06］=erl (Weiberl)</Link></div><div class="col-2">115</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0120">［07］kleinを用ひる例</Link></div><div class="col-2">120</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0128">［08］Waegelchen等、Grärtchen.</Link></div><div class="col-2">128</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0134">［09］縮小名詞の意味、及ぴ例外</Link></div><div class="col-2">134</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0147">［10］=leinchen</Link></div><div class="col-2">147</div></div>
+<div class="row"><div class="col-2"> </div><div class="col-8"><Link to="../../002/s0148">［11］Umlautなき例 (Fauchen)</Link></div><div class="col-2">148</div></div>
+<div class="row"><div class="col-2">02-5B</div><div class="col-8"><Link to="../../002/s0149">Schwimmer型 Schnitzwe型</Link></div><div class="col-2">149-163</div></div>
+<div class="row"><div class="col-2">02-6B</div><div class="col-8"><Link to="../../002/s0164">Der Hebel型 男性名詞</Link></div><div class="col-2">164-167</div></div>
+<div class="row"><div class="col-2">02-7B</div><div class="col-8"><Link to="../../002/s0168">Physiognomik statt Physiognomie</Link></div><div class="col-2">168-75</div></div>
+<div class="row"><div class="col-2">02-8B</div><div class="col-8"><Link to="../../002/s0176">抽象名詞</Link></div><div class="col-2">176-197</div></div>
+<div class="row"><div class="col-2">02-9B</div><div class="col-8"><Link to="../../002/s0198">Ge= [集合名詞］ Ge=stein型</Link></div><div class="col-2">198-203</div></div>
+<div class="row"><div class="col-2">02-10B</div><div class="col-8"><Link to="../../002/s0204">=ierung と =ation</Link></div><div class="col-2">204</div></div>
+<div class="row"><div class="col-2">02-11B</div><div class="col-8"><Link to="../../002/s0205">動詞の語幹に相当する男性名詞</Link></div><div class="col-2">205</div></div>
+<div class="row"><div class="col-2">02-12B</div><div class="col-8"><Link to="../../002/s0206">稀なもの、der Emphehl 等</Link></div><div class="col-2">206</div></div>
+<div class="row"><div class="col-2">02-13B</div><div class="col-8"><Link to="../../002/s0207">自由型貶詞 für Sachen貶詞をも含む 事と物と人と<br />、名詞の性に関する諸種の疑問な事例</Link></div><div class="col-2">207-213<br /> </div></div>
+<div class="row"><div class="col-2">02-14B</div><div class="col-8"><Link to="../../002/s0214">欧一般の癖、固有名詞で一般問題を論ずる</Link></div><div class="col-2">214-215</div></div>
+<div class="row"><div class="col-2">02-15R</div><div class="col-8"><Link to="../../002/s0216">Nomen agentisで文意をGrand coureur de filles.</Link></div><div class="col-2">216-232</div></div>
+<div class="row"><div class="col-2">02-16B</div><div class="col-8"><Link to="../../002/s0233">外来語（主として名詞）</Link></div><div class="col-2">233</div></div>
+</div>
+  </Layout>
+)
+
+export default SecondPage
---
+++ src/pages/002/s0149.js
@@ -0,0 +1,56 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <Seo title="関口存男文例集総目次" />
+     <div class="container">
+
+<div class="row text-center">
+<div class="col-4">
+      <Link to="../s0148">←前ページ</Link>
+</div>
+<div class="col-4">
+      <Link to="/000/s0003">↑</Link>
+</div>
+<div class="col-4">
+      <Link to="../s0150">後ページ→</Link>
+</div>
+</div><nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">原本</a>
+    <a class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" href="#nav-honkoku" role="tab" aria-controls="nav-profile" aria-selected="false">翻刻</a>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+      <StaticImage
+        src="../../images/002/s0149.png"
+      />
+</div>
+  <div class="tab-pane fade" id="nav-honkoku" role="tabpanel" aria-labelledby="nav-profile-tab">
+<p>
+鋭意製作中
+</p>
+</div>
+</div><div class="row text-center">
+<div class="col-4">
+      <Link to="../s0148">←前ページ</Link>
+</div>
+<div class="col-4">
+      <Link to="/000/s0003">↑</Link>
+</div>
+<div class="col-4">
+      <Link to="../s0150">後ページ→</Link>
+</div>
+</div>
+
+</div>
+  </Layout>
+)
+
+export default SecondPage
